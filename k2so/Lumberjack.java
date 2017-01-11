ebfab4d7417114afbbfbe1ffc5475f6ee248486d
Alli <allip158@gmail.com>
2017-01-10 19:46:54 -0500

    added some lumberjack functionality

--- k2so/Lumberjack.java
+++ k2so/Lumberjack.java
@@ -48,14 +48,18 @@ public class Lumberjack extends DefaultRobot{
 
 	private void shakeThenChop(TreeInfo[] neutralTrees) throws GameActionException{
 		
-		MapLocation treeLocation = neutralTrees[0].getLocation();
-    	if (rc.canShake()) {
+		TreeInfo tree = neutralTrees[0];
+		MapLocation treeLocation = tree.getLocation();
+		
+    	if (rc.canShake() && tree.containedBullets > 0) {
     		
     		rc.shake(treeLocation);
     		
-    	} else if(rc.canChop(treeLocation)) {
+    	} else if(rc.canChop(treeLocation) && tree.containedRobot != null) {
     	
     		rc.chop(treeLocation);
+    	} else {
+    		tryMove(randomDirection());
     	}
 		
 	}

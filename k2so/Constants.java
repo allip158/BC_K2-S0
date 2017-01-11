f82baa3ad11be3bcf0e9042af29930004113f598
Alli <allip158@gmail.com>
2017-01-10 21:45:35 -0500

    Added Constants and Utils files

---
+++ k2so/Constants.java
@@ -0,0 +1,8 @@
+package k2so;
+
+public final class Constants {
+	
+	// Number of Scouts per Archon at start of game
+	public static final int NUM_SCOUTS = 3;
+	
+}
---
+++ k2so/Utils.java
@@ -0,0 +1,10 @@
+package k2so;
+import battlecode.common.*;
+
+public final class Utils {
+	
+	public static void example() {
+		return;
+	}
+	
+}

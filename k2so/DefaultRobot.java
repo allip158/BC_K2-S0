4d9db26182f75de0f5eeab6dfbce38a43dc7b50d
Alli <allip158@gmail.com>
2017-01-24 22:44:57 -0500

    we can now specify number of each type of robot

--- k2so/DefaultRobot.java
+++ k2so/DefaultRobot.java
@@ -3,25 +3,65 @@ import battlecode.common.*;
 
 import java.util.Random;
 
+// TODO: clear out death array when full
+// TODO: same robot should only write in one array location
+
 public strictfp class DefaultRobot {
 	static RobotController rc;
 	static RobotType rt;
 	static Random rand;
-	
-	public DefaultRobot(RobotController rc){
+		
+	public DefaultRobot(RobotController rc) throws GameActionException{
 		this.rc = rc;
 		rand = new Random(rc.getID());
 		rt = rc.getType();
 	}
-	
+
 	public void run() throws GameActionException{
+		donateToWin();
 		executeTurn();
+//		sendDeathSignal();
 	}
 	
 	public void executeTurn() throws GameActionException{
 		tryMove(randomDirection());
 		return;
 	}
+	
+	/**
+	 * 
+	 * @return false if dying but no room in deathArray, true otherwise
+	 * @throws GameActionException
+	 */
+	public Boolean sendDeathSignal() throws GameActionException {
+		
+		float health = rc.getHealth();
+		int maxHealth = rc.getType().maxHealth;
+		
+		float percentHealth = (health / (float)maxHealth) * (float)100;
+		
+		if (percentHealth < Constants.PERCENT_HEALTH_FOR_DEATH_SIGNAL) {
+			
+			/* Send Death Signal */
+			int i = Constants.DEATH_ARRAY_START_INDEX;
+			while (i < Constants.DEATH_ARRAY_END_INDEX) {
+				
+				System.out.println("I am a " + rc.getType() + " and I am dying!");
+				
+				int existingSignal = rc.readBroadcastInt(i);
+				int signal = Utils.getSignalFromRobotType(rc.getType());
+				
+				if (existingSignal != 0) {
+					rc.broadcastInt(i, signal);
+					return true;
+				}
+				i++;
+			}
+			return false;
+		}
+		return true;
+	}
+	
 	
 	public void donateToWin() throws GameActionException {
 		float bullets = rc.getTeamBullets();
